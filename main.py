e733c98fc2a80de4855eb3e0a5529c7d1df464c1
Juan Pablo Quiroga <juanpquiroga@gmail.com>
2023-04-16 16:15:42 +0000

    Add pandas

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 import streamlit as st
+import pandas as pd
+
+data = {'Series_1': [1, 3, 4, 5, 7], 'Series_2': [10, 30, 40, 100, 250]}
+
+df = pd.DataFrame(data)
 
 st.title('Our streamlit app')
 st.subheader('Introducing streamlith with automate everything with python')
@@ -6,3 +11,5 @@ st.write('''
 This is our first web app.
 Enjoy it.
 ''')
+
+st.write(df)
