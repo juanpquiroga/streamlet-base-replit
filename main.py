76ae3673c91ebaf6f7991ece49894d39b5cc6780
JuanPabloPabl24 <2074604-JuanPabloPabl24@users.noreply.replit.com>
2023-04-16 15:48:54 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.title('Our streamlit app')
+st.subheader('Introducing streamlith with automate everything with python')
+st.write('''
+This is our first web app.
+Enjoy it.
+''')
